df2afbf2cb5eca06ba56f4c81a79c69e98b99faa
IosDevelopUZ <126049891+IosDevelopUZ@users.noreply.github.com>
2023-05-30 11:34:39 +0500

    Initial Commit

---
+++ To Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To Do List
+//
+//  Created by Khalmatov on 30.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
